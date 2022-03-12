2ad93820eec5f07ff966916b7b1a50ee9668ab26
robercj <38355972+robercj@users.noreply.github.com>
2022-03-12 11:11:38 -0500

    React Router additions and corrections

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Directory extends Component {
 
-function RenderDirectoryItem(campsite, onClick) {
+function RenderDirectoryItem({ campsite }) {
     return (
-        <Card onClick={ () => onClick(campsite.id)}>
+        <Card>
             <CardImg width="100%" src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
                 <CardTitle>{campsite.name}</CardTitle>
@@ -18,7 +17,7 @@ function Directory(props) {
         const directory = props.campsites.map(campsite => {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <RenderDirectoryItem campsite={campsite} onClick={props.onClick}/>
+                    <RenderDirectoryItem campsite={campsite} />
                 </div>
             );
         });
@@ -29,8 +28,7 @@ function Directory(props) {
                     {directory}
                 </div>
             </div>
-        );
-    }
+    );
 }
 
 export default Directory;
