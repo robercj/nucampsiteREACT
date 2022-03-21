fd2542bfca9324b77558195429b264dca1988337
robercj <38355972+robercj@users.noreply.github.com>
2022-03-20 20:20:59 -0400

    Add comment modal

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {ModalBody, Form, FormGroup, Input, Label, Col, Row, Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader } from 'reactstrap';
+import {ModalBody, Label, Col, Row, Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
